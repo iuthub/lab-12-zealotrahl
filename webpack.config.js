cf6536f0c05522b1118d49b31d2b18a549369645
Kamoliddin Rikhsiboev <komolonski@bk.ru>
2019-05-11 01:10:46 +0500

    Lab done

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const config = {
+	entry : './src/index.js',
+	output : {
+		path : path.resolve(__dirname, 'dist'),
+		filename : 'bundle.js'
+	},
+	module : {
+		rules : [
+			{
+				test : /\.(scss)$/,
+				use : [
+					{
+						loader : 'style-loader'
+					},
+					{
+						loader : 'css-loader'
+					},
+					{
+						loader : 'postcss-loader',
+						options : {
+							plugins : function () {
+								return [
+									require ('autoprefixer')
+								];
+							}
+						}
+					},
+					{
+						loader : 'sass-loader'
+					}
+				]
+			},
+
+			{
+				test : /\.css$/,
+				use : [
+					'style-loader',
+					'css-loader'
+				]
+			},
+			{
+				test : /\.html$/,
+				use : 'html-loader'
+			},
+			{
+				test : /\.(png|jpe?g|gif)$/,
+				use : 'file-loader'
+			}
+		]
+	},
+
+	plugins : [
+		new HtmlWebpackPlugin ({
+			template : "./src/index.html"
+		})
+	]
+}
+module.exports = config;
